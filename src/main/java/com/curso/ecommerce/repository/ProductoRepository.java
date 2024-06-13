19f5064fee916ad12b1610a1bd0269fc603f4d70
ch3p3l1t0 <chepelito2016@gmail.com>
2024-06-13 16:30:39 -0500

    Crear el repository y clase servicio CRUD producto

---
+++ src/main/java/com/curso/ecommerce/repository/ProductoRepository.java
@@ -0,0 +1,12 @@
+package com.curso.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.yaml.snakeyaml.events.Event.ID;
+
+import com.curso.ecommerce.model.Producto;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer>{
+
+}
