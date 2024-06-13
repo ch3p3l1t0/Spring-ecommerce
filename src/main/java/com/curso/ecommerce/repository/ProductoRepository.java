a836994fe51efe36e5ce12c57a12d5f346f46ca1
ch3p3l1t0 <chepelito2016@gmail.com>
2024-06-13 17:52:42 -0500

    Crear el controller Producto y la vista ver productos

--- src/main/java/com/curso/ecommerce/repository/ProductoRepository.java
+++ src/main/java/com/curso/ecommerce/repository/ProductoRepository.java
@@ -2,7 +2,6 @@ package com.curso.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.yaml.snakeyaml.events.Event.ID;
 
 import com.curso.ecommerce.model.Producto;
 
