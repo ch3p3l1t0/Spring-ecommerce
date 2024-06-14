071f83d93e0f62759e8110957471c100f7eee3a6
ch3p3l1t0 <chepelito2016@gmail.com>
2024-06-13 19:49:17 -0500

    Implementar la funcionalidad mostrar productos administrador

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -3,6 +3,7 @@ package com.curso.ecommerce.controller;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,8 @@ public class ProductoController {
     private ProductoService productoService;
 
     @GetMapping("")
-    public String show(){
+    public String show(Model model){
+        model.addAttribute("productos", productoService.findAll());
         return "productos/show";
     }
     @GetMapping("/create")
