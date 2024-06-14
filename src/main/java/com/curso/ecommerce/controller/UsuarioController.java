b5910c4915ec28f11f579d7bc2ff525d69689e15
ch3p3l1t0 <chepelito2016@gmail.com>
2024-06-14 18:13:59 -0500

    Autenticación de usuario temporal

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.curso.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.service.IUsuarioService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/usuario")
@@ -35,4 +39,30 @@ public class UsuarioController {
 		return "redirect:/";
 	}
 
+    @GetMapping("/login")
+	public String login() {
+		return "usuario/login";
+	}
+
+    @PostMapping("/acceder")
+	public String acceder(Usuario usuario, HttpSession session) {
+		logger.info("Accesos : {}", usuario);
+		
+		Optional<Usuario> user=usuarioService.findByEmail(usuario.getEmail());
+		//logger.info("Usuario de db: {}", user.get());
+		
+		if (user.isPresent()) {
+			session.setAttribute("idusuario", user.get().getId());
+			
+			if (user.get().getTipo().equals("ADMIN")) {
+				return "redirect:/administrador";
+			}else {
+				return "redirect:/";
+			}
+		}else {
+			logger.info("Usuario no existe");
+		}
+		
+		return "redirect:/";
+	}
 }
